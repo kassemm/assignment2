c68b4932753615e8c142f0d2801707c06faac396
kassemm <moustafa.m.kassem@gmail.com>
2014-10-01 23:20:11 +0200

    Update Bakery.java

--- Bakery.java
+++ Bakery.java
@@ -11,11 +11,14 @@ class Bakery implements Lock{
 	--
 	The solution is to use the volatile for the shared variables so that any values read or written to these 
 	shared variable will be from the main memory so that always the updated values is used. 
-	
+	After that  a shared volatile variable is declared "ready" which is set to true after being written 
+	by a thread and when needed to be read by a thread this thread should check for is the shared variable is true or no
+	if true it will read from the shared variable and in this way it will be sure that latest value of the variable is read.	
 	*/
 	
-	volatile boolean  ready ;				
-	volatile boolean     []     flag ;  
+	
+	volatile boolean  ready ;	
+	volatile boolean     []    flag ;  
 	volatile int[]   	 	   label;  
 	int[]	    label_sorted;
 	
@@ -32,6 +35,7 @@ class Bakery implements Lock{
 			flag[i] = false;
 			label[i] =0;
 		}
+		ready = false;
 	}
 
 	public void lock(){
@@ -64,18 +68,21 @@ class Bakery implements Lock{
 		}
 		Arrays.sort(label_sorted);
 		
+		
 		//Increase by one
-
 		if(label[i] !=0)
 		{
 			if(label[i] != label_sorted[label_sorted.length-1] )
 			{
+				//Write to shared variable
 				label[i] =label_sorted[label_sorted.length-1] +1;
+				ready = true;
 			}
 		}
 		else
-		{
+		{	//Write to shared variable
 			label[i] =label_sorted[label_sorted.length-1] +1;
+			ready = true;
 		}
 		
 		/*********Debug************/
@@ -90,7 +97,7 @@ class Bakery implements Lock{
 			System.out.println(flag[j]);
 		}
 		/*********Debug************/
-		
+
 		//Read all the labels and make sure that there is no thread with lower ID than the current thread otherwise block
 		for (int k =0; k < this.number_threads ;k++)
 		{
@@ -101,24 +108,30 @@ class Bakery implements Lock{
 				for(int l=0; l<label.length;l++){System.out.println(label[l]);}
 				for(int l=0; l<label.length;l++){System.out.println(flag[l]);}
 				/*********Debug************/
-				while( (flag[k] ==true) && (label[k] <= label[i]) );
+				//Read from shared variable
+				if (ready == true){	while( (flag[k] ==true) && (label[k] <= label[i]) ); };
 				System.out.printf("Exit wait thread %d \n",(i+1));
 				
-				if((flag[i] ==true) )
-				{
-					if(flag[k] == true)
-					{
-						if(label[k] > label[i])
+				if (ready == true){
+					//Read from shared variable
+					if((flag[i] ==true) )
+					{	
+						//Read from shared variable
+						if(flag[k] == true)
+						{	
+							//Read from shared variable
+							if(label[k] > label[i])
+							{
+								System.out.printf("Thread %d enters CS\n",(i+1));
+							}
+						}
+						else
 						{
 							System.out.printf("Thread %d enters CS\n",(i+1));
+							
 						}
-					}
-					else
-					{
-						System.out.printf("Thread %d enters CS\n",(i+1));
 						
 					}
-					
 				}
 			}
 		}
